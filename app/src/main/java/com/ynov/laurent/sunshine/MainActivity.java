e639ee4e3cd032aa546f8fda19f8d32e06d3a216
Laurent GAIDON <laurent.gaidon@hins.fr>
2016-11-12 18:57:51 +0100

    created Sunshine Project - Added ListView

---
+++ app/src/main/java/com/ynov/laurent/sunshine/MainActivity.java
@@ -0,0 +1,50 @@
+package com.ynov.laurent.sunshine;
+
+import android.content.Context;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayAdapter<String> mForecastAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String[] data = {"Mon 6/23 - Sunny - 31/17",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Tue 6/24 - Foggy - 21/8",
+                "Wed 6/25 - Cloudy - 22/17",
+                "Thurs 6/26 - Rainy - 18/11",
+                "Fri 6/27 - Foggy - 21/10",
+                "Sat 6/28 - TRAPPED IN WEATHERSTATION - 23/18",
+                "Sun 6/29 - Sunny - 20/7"};
+
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
+        mForecastAdapter = new ArrayAdapter<String>(this,
+                R.layout.list_item_forecast,
+                R.id.list_item_forecast_textview,
+                weekForecast);
+
+
+        ListView listView = (ListView) findViewById(R.id.list_view_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+    }
+}
